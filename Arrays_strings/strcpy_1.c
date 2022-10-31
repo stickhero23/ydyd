dfb1ae3c63a04c841fcb7c011432e1c6a6f97d4b
stickhero23 <stickhero23@gmail.com>
2022-10-31 07:41:24 +0300

    Arrays and Strings Exercises

---
+++ Arrays_strings/strcpy_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * @brief Copies strings using strcpy
+ * 
+ */
+char main()
+{
+    int i;
+
+    char dest = "Good Morning";
+    char src = "Welcome to Class.";
+
+    
+
+}
